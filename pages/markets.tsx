e4bff5ce8c19a8a3c50ac769e59391e284f95e67
Argha Chakrabarty <ac380012@gmail.com>
2021-08-05 03:59:33 +0530

    grid layout in market page

--- pages/markets.tsx
+++ pages/markets.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import Head from "next/head";
+
 import Sidenav from "@components/sidenav";
+import styles from "@styles/market.module.scss";
 
 const Markets = ():JSX.Element => {
 	return (
-		<div style={{minHeight: "200vh"}}>
+		<div>
 			<Head>
 				<title>Mock Stock Market</title>
 			</Head>
 
 			<Sidenav />
 			
-			<div>
-				hello
+			<div className={`${styles.container}`}>
+				<div className={`${styles.notification} ${styles.section}`}>
+					notification
+				</div>
+
+				<div className={`${styles.portfolio} ${styles.section}`}>
+					portfolio
+				</div>
+				
+				<div className={`${styles.market} ${styles.section}`}>
+					market
+				</div>
+				
+				<div className={`${styles.leader} ${styles.section}`}>
+					leader
+				</div>
 			</div>
 
 		</div>
