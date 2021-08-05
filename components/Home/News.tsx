f478064b1df28a69f8c1758b715d9c8010c2d55f
Niranjan420 <niranjanrajesh02@gmail.com>
2021-08-05 14:36:44 +0530

    dynamic news

--- components/Home/News.tsx
+++ components/Home/News.tsx
@@ -1,42 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "@styles/home.module.scss"
 import { RightArrow, LeftArrow, NewsIcon } from "@components/icons";
+import NewsData from "../../data/news"
+import Loading from "@components/loading";
+interface NewsMain {
+  time: string;
+  serialNo: number;
+  headlines: string[];
+	new?: boolean;
+}
+
 const News = ():JSX.Element => {
+	console.log(NewsData);
+	//newsLimit is the number of news updates that has been done so far (different from final limit)
+	const newsLimit:number = 4;
+	const [newsNo, setNewsNo] = useState<number>(1)
+	const [currentArticle, setCurrentArticle] = useState<NewsMain | null>(null)
+
+	useEffect(() => {
+		setCurrentArticle(NewsData[newsNo-1])
+	}, [newsNo])
+
+	function increaseNewsNo() {
+		if (newsNo >= newsLimit) {
+			return
+		}
+		else {
+			setNewsNo(newsNo+1)
+		}
+	}
+	function decreaseNewsNo() {
+		if (newsNo <= 1) {
+			return
+		}
+		else {
+			setNewsNo(newsNo-1)
+		}
+	}
+
   return(
     <div className={styles.newsContainer}>
-			<div className={styles.headerContainer}>
-				<div className={styles.headerLeft}>
-					<div>
-						<span >
-							<NewsIcon />	
-						</span>
-						<h1>News</h1>
+			{currentArticle && (
+			<>
+				<div className={styles.headerContainer}>
+					<div className={styles.headerLeft}>
+						<div>
+							<span >
+								<NewsIcon />	
+							</span>
+							<h1>News</h1>
+						</div>
+						<h5>Next news update in 10 seconds ({newsLimit}/12)</h5>
 					</div>
-					<h5>Next news update in 10 seconds (5/12)</h5>
-				</div>
-				<div className={styles.headerRight}>
-					<h5>9:00 pm</h5>
-					<div>
-					NEW
+					<div className={styles.headerRight}>
+						<h5>{currentArticle.time}</h5>
+						{currentArticle.new && (
+							<div>
+							NEW
+							</div>
+						)}
 					</div>
 				</div>
-			</div>
-			<hr/>
-			<div className={styles.newsBody}>
-			<ul>
-				<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, at.</li>
-				<li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo nisi corrupti quidem delectus possimus rem similique exercitationem! Corporis, inventore beatae?</li>
-				<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, at.</li>
-				<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem recusandae minus ratione tempora consectetur numquam dolorum nam quo, alias quia!</li>						
-				<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, at.</li>
-				<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem recusandae minus ratione tempora consectetur numquam dolorum nam quo, alias quia!</li>						
-			</ul>
-			</div>
-			<div className={styles.arrowContainer}>
-				<LeftArrow />
-				<p>5/5</p>
-				<RightArrow />
-			</div>
+				<hr/>
+				<div className={styles.newsBody}>
+				<ul>
+					{currentArticle.headlines.map(item => <li>{item}</li>)}
+			
+				</ul>
+				</div>
+				<div className={styles.arrowContainer}>
+					<a onClick={decreaseNewsNo}>
+						<LeftArrow />
+					</a>
+					<p>{newsNo}/{newsLimit}</p>
+					<a onClick={increaseNewsNo}>
+						<RightArrow />
+					</a>
+				</div>
+			</>
+			)}
+			{!currentArticle && (
+				<>
+					<Loading />
+				</>
+			)}
 		</div>
   )
 }
