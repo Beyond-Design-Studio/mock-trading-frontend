37da15a27a67bbbf2e2b9ad7b5d130bf0704f647
Aarav Varshney <aaravvarshney22@gmail.com>
2021-09-05 23:29:03 +0530

    modify config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  webpack: (config, options) => {
+    // Important: return the modified config
+    config.externals = [...config.externals, 'bufferutil', 'utf-8-validate']
+    return config
+  },
   reactStrictMode: true,
   images: {
     domains: ["avatars.dicebear.com"]
