1a6c5ff9fbffa8653117f92697266aabcc51c7fb
Aarav Varshney <aaravvarshney22@gmail.com>
2021-10-18 15:22:41 +0530

    content change on about us

--- components/Home/AboutUs.tsx
+++ components/Home/AboutUs.tsx
@@ -1,37 +1,57 @@
 import React from "react";
 import styles from "@styles/home.module.scss"
 import { InfoIcon } from "@components/icons";
-const AboutUs = ():JSX.Element => {
-  return(
+const AboutUs = (): JSX.Element => {
+  return (
     <div className={styles.aboutUsContainer}>
       <div className={styles.containerHeader}>
         <span>
-          <InfoIcon/>
+          <InfoIcon />
         </span>
 
         <h1>About Us</h1>
       </div>
 
-      <div style={{width: "100%", height: "2px", backgroundColor: "var(--accent-color)"}}></div>
+      <div style={{ width: "100%", height: "2px", backgroundColor: "var(--accent-color)" }}></div>
 
       <div className={styles.aboutBody}>
         <div className={styles.aboutSection}>
-          <h2>Bodhi Capital</h2>
+          <h2><a href="https://tinyurl.com/ashokainv" target="_blank" rel="noreferrer">Ashoka Investments Club </a></h2>
           <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ducimus repellendus quasi pariatur consequatur dolore culpa temporibus, eligendi natus, itaque eaque voluptatibus nemo alias. Cumque tempore suscipit quibusdam cupiditate labore soluta maxime quos iusto? Ullam, officiis corrupti ex beatae sequi saepe sit veritatis asperiores, recusandae facere similique vel possimus tempora.</p>
+            <p>AIC, the investments club of Ashoka University, consists of finance enthusiasts committed to
+              establishing an investment ethos in a liberal arts environment at Ashoka. We meet regularly to discuss stock
+              picks and investment philosophies, along with hosting speaker sessions on different aspects of investing.
+              Our aim is to establish a working knowledge of personal finance in our surroundings, by organising competitions
+              like the MockStock, Cryptic Hunts, etc. We also run India’s first student-led undergraduate fund -
+              <b><a href="https://bodhicapital.in/" target="_blank" rel="noreferrer">Bodhi Capital </a></b>. It was founded and incorporated as a partnership firm in 2019. Led by a team of 7 Portfolio Managers and 16 Analysts to make key investment decisions,
+              the vision of Bodhi is to produce superior risk-adjusted returns through equity research based on bottom-up fundamental analysis.</p>
           </div>
         </div>
 
-        <div className={styles.aboutLine}/>
-        
+        <div className={styles.aboutLine} />
+
         <div className={styles.aboutSection}>
-          <h2>Ashoka Investments Club</h2>
+          <h2>
+            <a href=" https://www.ashoka.edu.in/CFE" target="_blank" rel="noreferrer">
+              InfoEdge Centre for Entrepreneurship
+            </a>
+          </h2>
           <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero placeat explicabo accusantium inventore ratione, fuga fugiat esse optio tempore ab totam facere veritatis asperiores vel ut repudiandae! Necessitatibus, unde corrupti! Labore adipisci deleniti cum ut repudiandae mollitia vitae quis, minima nisi ex, minus corporis sunt accusantium laudantium ipsa iure. In!</p>
+            <p>
+              The InfoEdge Centre for Entrepreneurship is the creative hub at Ashoka University that nurtures
+              the entrepreneurial endeavours of all students, fellows, faculty and founders alike. With the
+              mission of creating a strong foundation of academic learning in the field of entrepreneurship,
+              and inculcating a culture of innovation and entrepreneurial spirit on campus, The Centre is committed
+              to assisting students in every stage of the lifecycle of an enterprise– from inspiration, ideation and
+              initiation, to incubation, investments and internationalisation for both mainstream innovations as well
+              as ventures around social innovation and sustainability. The CFE is the perfect amalgamation of Liberal Arts and
+              Entrepreneurship, facilitating a vibrant community of Entrepreneurs to work in a wide range of sectors spanning
+              from the mainstream to the niche sectors.
+            </p>
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 export default AboutUs;
--- components/Home/Rules.tsx
+++ components/Home/Rules.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "@styles/home.module.scss"
 import { RulesIcon } from "@components/icons";
-const Rules = ():JSX.Element => {
-  return(
+const Rules = (): JSX.Element => {
+  return (
     <div className={styles.rulesContainer}>
       <div className={styles.containerHeader}>
         <span>
@@ -11,21 +11,23 @@ const Rules = ():JSX.Element => {
         <h1>Rules</h1>
       </div>
 
-      <div style={{width: "100%", height: "2px", backgroundColor: "var(--accent-color)"}}></div>
+      <div style={{ width: "100%", height: "2px", backgroundColor: "var(--accent-color)" }}></div>
 
       <div>
-      <ul>
-        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, amet.</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugit fugiat unde laborum veniam debitis!</li>
-        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, amet.</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugit fugiat unde laborum veniam debitis!</li>
-        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, amet.</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugit fugiat unde laborum veniam debitis!</li>
-        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, amet.</li>
-        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, amet.</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugit fugiat unde laborum veniam debitis!</li>
-        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, amet.</li>
-      </ul>		
+        <ul>
+          <li>The stock market will be open for <b> 80 minutes for a total of 20 rounds. Each round shall be 4 minutes long. </b></li>
+          <li>There will be a new set of news updates at the beginning of each round on the basis of which participants should make their investing decisions.</li>
+          <li>Each individual will have <b>Rs. 10,00,000</b>to invest.</li>
+          <li>For rounds 1-10, investors may only buy <b>Rs. 5,00,000 worth of one stock, Rs. 2,00,000 worth of one cryptocurrency, and Rs. 2,00,000 worth of one commodity in each round.
+          </b></li>
+          <li>For rounds 11-20, the investment limits shall be increased to <b>Rs. 7,50,000 for a particular stock, Rs. 5,00,000 for a particular cryptocurrency, and Rs. 5,00,000 for a particular commodity in each round.</b></li>
+          <li>Stocks, cryptocurrencies, and commodities may be bought and sold at any given time</li>
+          <li>At the end of the event the individuals with the 3 highest net worths will be declared as the winners.
+          </li>
+          <li>Use of calculators is permitted. </li>
+          <li>Use of the internet is permitted. </li>
+          <li>In case of technical difficulties, please contact any of the organisers at the event helpdesk.</li>
+        </ul>
       </div>
     </div>
   )
