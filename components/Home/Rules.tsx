61508e1adb03e8e8f59b3e8e6b54ff0e7c8e2335
Aarav Varshney <aaravvarshney22@gmail.com>
2021-10-23 13:14:42 +0530

    change in rules

--- components/Home/Rules.tsx
+++ components/Home/Rules.tsx
@@ -17,7 +17,7 @@ const Rules = (): JSX.Element => {
         <ul>
           <li>The stock market will be open for <b> 80 minutes for a total of 20 rounds. Each round shall be 4 minutes long. </b></li>
           <li>There will be a new set of news updates at the beginning of each round on the basis of which participants should make their investing decisions.</li>
-          <li>Each individual will have <b>Rs. 10,00,000</b>to invest.</li>
+          <li>Each individual will have <b>Rs. 10,00,000</b> to invest.</li>
           <li>For rounds 1-10, the investment limits are <b>Rs. 5,00,000 per stock, Rs. 2,00,000 per cryptocurrency, and Rs. 2,00,000 per commodity in each round. </b></li>
           <li>For rounds 11-20, the investment limits shall be increased to <b>Rs. 7,50,000 per stock, Rs. 5,00,000 per cryptocurrency, and Rs. 5,00,000 per commodity in each round.</b></li>
           <li>Investors may buy/sell any number of stocks, cryptocurrencies, and commodities at any time during the event.</li>
@@ -25,7 +25,7 @@ const Rules = (): JSX.Element => {
           </li>
           <li>Use of calculators is permitted. </li>
           <li>Use of the internet is permitted. </li>
-          <li>In case of technical difficulties, please contact the organisers at the event <a href='https://zoom.us/j/94677452623?pwd=R1ZkNWh0c2VSN2ZxbGdISWl6NW5CUT09' >helpdesk.</a></li>
+          <li>In case of technical difficulties, please contact the organisers at the event <a href='https://zoom.us/j/94677452623?pwd=R1ZkNWh0c2VSN2ZxbGdISWl6NW5CUT09' target="_blank" rel="noreferrer" style={{ 'textDecoration': 'underline', color: '#0645AD' }}>helpdesk.</a></li>
         </ul>
       </div>
     </div>
